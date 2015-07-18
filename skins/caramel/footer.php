3820dfed36883594c652cfdeb310ae2597c5c2c4
花木カズキ <kazukikf@gmail.com>
2015-07-18 19:38:48 +1000

    Updated Caramel

--- skins/caramel/footer.php
+++ skins/caramel/footer.php
@@ -1,2 +1,2 @@
 <script src="{{skinURL}}/js/jquery-2.1.3.min.js"></script>
-<script src="{{skinURL}}/js/Caramel.js" type="text/javascript"></script>
+<script src="{{skinURL}}/js/caramel.min.js" type="text/javascript"></script>
